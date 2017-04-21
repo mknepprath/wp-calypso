78ea990a9c82e5292a5309941381054209bb9b23
oskosk <oskosk@gmail.com>
2017-04-21 08:48:29 -0300

    Remove usage of site object methods from section-security.jsx

--- client/my-sites/site-settings/section-security.jsx
+++ client/my-sites/site-settings/section-security.jsx
@@ -24,7 +24,7 @@ const SiteSettingsSecurity = ( { site, translate } ) => {
 		);
 	}
 
-	if ( ! site.canManage() ) {
+	if ( ! site.canManage ) {
 		return (
 			<JetpackManageErrorPage
 				template="optInManage"
@@ -35,7 +35,7 @@ const SiteSettingsSecurity = ( { site, translate } ) => {
 		);
 	}
 
-	if ( ! site.versionCompare( '3.4', '>=' ) ) {
+	if ( ! site.hasMinimumJetpackVersion ) {
 		return (
 			<JetpackManageErrorPage
 				template="updateJetpack"
